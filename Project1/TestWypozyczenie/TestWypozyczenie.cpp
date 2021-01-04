801278716b37fb11ed1e3dee5d56577b57546bb4
KACPERDZW\User <kbielak@poczta.agh.edu.pl>
2021-01-04 21:28:19 +0100

    Niepotrzebne pliki.

---
+++ Project1/TestWypozyczenie/TestWypozyczenie.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project1/Wypozyczenie.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace TestWypozyczenie
+{
+	TEST_CLASS(TestWypozyczenie)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			string numer = "LJA1234";
+
+		}
+	};
+}
