2a31dc01f7c591059b313b3b3f132b30eec656a1
KACPERDZW\User <kbielak@poczta.agh.edu.pl>
2021-01-09 14:18:48 +0100

    Faza Red - test do metody zmien_cene w klasie Pojazd.

---
+++ Project1/TestZmienCene/TestZmienCene.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project1/Pojazd.h"
+#include "../Project1/Pojazd.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestZmienCene
+{
+	TEST_CLASS(TestZmienCene)
+	{
+	public:
+		
+		TEST_METHOD(TestZmienCeneWPojazd)
+		{
+			Pojazd p;
+			p.Cena_za_godzine = 120;
+			int nowa_cena = 150;
+			p.zmien_cene(nowa_cena);
+			Assert::AreEqual(p.Cena_za_godzine, nowa_cena);
+
+		}
+	};
+}
